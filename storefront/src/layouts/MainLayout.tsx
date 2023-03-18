ea8629081617bc24a6b7da467da0429f3d40e418
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-03-18 09:54:44 +0000

    build(npm packages and config on storefront): tailwind, toast,tanstack,cypress
    npm packages and config

--- storefront/src/layouts/MainLayout.tsx
+++ storefront/src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { PropsWithChildren } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 export default function MainLayout({
   children,
@@ -13,6 +14,7 @@ export default function MainLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Toaster/>
       {children}
     </>
   );
--- storefront/src/pages/index.tsx
+++ storefront/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,9 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <div>
-      No store selected home
-     </div>
+      <div>No store selected home</div>
     </>
-  )
+  );
 }
