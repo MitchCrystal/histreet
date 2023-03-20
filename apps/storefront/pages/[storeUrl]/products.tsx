5c52678dae9d9fe95658407832a1e18f1552e860
jackb14 <jack@26385.xyz>
2023-03-20 13:16:51 +0000

    feat(product grid item): add product grid item and add to products page
    #50 & #44

--- apps/storefront/pages/[storeUrl]/products.tsx
+++ apps/storefront/pages/[storeUrl]/products.tsx
@@ -1,9 +1,29 @@
+import HeadingText from '../../components/HeadingText';
+import ProductGridItem from '../../components/ProductGridItem';
 import MainLayout from '../../layouts/MainLayout';
+
+const products = [
+  {
+    id: '1',
+    firstImage: 'https://picsum.photos/500/600',
+    secondImage: 'https://picsum.photos/500/700',
+    name: 'Product 1',
+    price: 100,
+    nameSlug: 'product-1',
+  },
+];
 
 function Products() {
   return (
     <>
-      <p>Products</p>
+      <HeadingText size="h3">Products</HeadingText>
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
+        {products.map((product) => (
+          <div>
+            <ProductGridItem key={product.id} product={product} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
