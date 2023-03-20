6b6df92e7381361b7c4195a77203b935f6d64726
jackb14 <jack@26385.xyz>
2023-03-20 10:27:26 +0000

    feat(storefront mainlayout): add storefront mainlayout and additional components
    #48

--- apps/storefront/layouts/MainLayout.tsx
+++ apps/storefront/layouts/MainLayout.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { PropsWithChildren } from 'react';
 import { Toaster } from 'react-hot-toast';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 export default function MainLayout({
   children,
@@ -15,7 +17,13 @@ export default function MainLayout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Toaster />
-      {children}
+      <div className="flex flex-col gap-8 justify-between min-h-screen">
+        <Navbar />
+        <div className="flex-1 min-h-full max-w-[1200px] m-auto w-full px-4">
+          {children}
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
