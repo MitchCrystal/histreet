9737dab61cb66aa8c743a21e5cbe9309a2eaa694
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-03-20 15:19:35 +0000

    feat(pages/editor): store editor boiler plate completed
    store editor boiler plate completed but awaiting tanstack functionality and mobile responsive
    
    #38

--- apps/admin/components/Heading.tsx
+++ apps/admin/components/Heading.tsx
@@ -12,17 +12,17 @@ export default function Heading({ title, type }: Props) {
         </h1>
       )}
       {type === 'h2' && (
-        <h2 className="mt-10 lg:mb-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors dark:border-b-slate-700">
+        <h2 className="lg:mb-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors dark:border-b-slate-700">
           {title}
         </h2>
       )}
       {type === 'h3' && (
-        <h3 className="mt-8 scroll-m-20 text-2xl font-semibold tracking-tight">
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
           {title}
         </h3>
       )}
       {type === 'h4' && (
-        <h4 className="mt-8 scroll-m-20 text-xl font-semibold tracking-tight">
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
           {title}
         </h4>
       )}
--- apps/admin/components/Textarea.tsx
+++ apps/admin/components/Textarea.tsx
@@ -1,8 +1,12 @@
+import * as React from 'react';
+import { Label } from './Label';
+
 type Props = {
   label: string;
   id: string;
-  state: Record<string,string>; 
-  setState: React.Dispatch<React.SetStateAction<Record<string,string>>>;
+  state: Record<string, string>;
+  setState: React.Dispatch<React.SetStateAction<any>>;
+  direction: 'row' | 'column';
   [x: string]: any;
 };
 
@@ -11,19 +15,31 @@ export default function Textarea({
   id,
   state,
   setState,
+  direction,
   ...delegated
 }: Props) {
   return (
-    <div className="Textarea">
-      <label htmlFor={id}>{label}</label>
-      <textarea
-        className=
-          'flex h-30 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-md  placeholder:text-black focus:outline-none focus:ring-2focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50'
-        id={id}
-        value={state[id]}
-        onChange={(e) => setState({ ...state, [id]: e.target.value })}
-        {...delegated}
-      />
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <div
+        className={`flex w-full ${
+          direction === 'row'
+            ? 'flex-row items-center justify-center'
+            : direction === 'column'
+            ? 'flex-col'
+            : ''
+        }`}
+      >
+        <Label htmlFor={id} className="w-48">
+          {label}
+        </Label>
+        <textarea
+          className="flex h-30 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-md  placeholder:text-black focus:outline-none focus:ring-2focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+          id={id}
+          value={state[id]}
+          onChange={(e) => setState({ ...state, [id]: e.target.value })}
+          {...delegated}
+        />
+      </div>
     </div>
   );
 }
