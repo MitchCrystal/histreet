236f9815cff29fc6a39c0010f0ad39ff10a81470
jackb14 <jack@26385.xyz>
2023-03-21 16:48:39 +0000

    feat(global cart context): add cart context to cart and nav

--- apps/storefront/components/CartLineItemTable.tsx
+++ apps/storefront/components/CartLineItemTable.tsx
@@ -1,6 +1,7 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { CartProduct } from '../pages/[storeUrl]/cart';
 import { InputWithLabel } from './InputWithLabel';
+import Loading from './Loading';
 
 const columns = [
   { id: 'item', name: 'Item' },
@@ -9,26 +10,58 @@ const columns = [
   { id: 'quantity', name: 'Quantity' },
   { id: 'total', name: 'Total' },
 ];
-const lineItems = [
-  {
-    id: 1,
-    name: 'Product 1',
-    nameSlug: 'product-1',
-    price: 100,
-    quantity: 1,
-    image: 'https://picsum.photos/500/600',
-  },
-];
 
 export default function CartLineItemTable({
   products,
+  cartContext,
+  setOrderTotal,
 }: {
   products: CartProduct;
+  cartContext: {
+    cartItems: { id: string; quantity: number }[];
+    setCartItems: React.Dispatch<React.SetStateAction<any>>;
+  };
+  setOrderTotal: React.Dispatch<React.SetStateAction<number>>;
 }) {
-  const [quantityValues, setQuantityValues] = useState<Record<string, number>>({
-    1: 1,
-    2: 1,
-  });
+  const [quantityValues, setQuantityValues] = useState<Record<string, number>>(
+    {}
+  );
+
+  console.log({ quantityValues });
+
+  useEffect(() => {
+    const obj: Record<string, number> = {};
+    products.forEach((item) => {
+      obj[item.product_id] =
+        cartContext.cartItems.find(
+          (cartItem) => cartItem.id === item.product_id
+        )?.quantity || 0;
+    });
+    setQuantityValues({ ...obj });
+  }, [products]);
+
+  console.log(cartContext.cartItems);
+
+  useEffect(() => {
+    setOrderTotal(
+      products
+        .map((item) => {
+          return item.product_price * quantityValues[item.product_id];
+        })
+        .reduce((acc, curr) => acc + curr, 0)
+    );
+    cartContext.setCartItems((prev: { id: string; quantity: number }[]) => {
+      const newCartItems = prev.map((item) => {
+        return {
+          ...item,
+          quantity: Number(quantityValues[item.id]),
+        };
+      });
+      return newCartItems;
+    });
+  }, [quantityValues]);
+
+  if (Object.entries(quantityValues).length === 0) return <Loading />;
 
   return (
     <div className="grid grid-cols-5 gap-6 items-center rounded-md bg-gray-50 p-8">
@@ -37,32 +70,41 @@ export default function CartLineItemTable({
           {column.name}
         </p>
       ))}
-      {products.map((lineItem) => (
-        <Fragment key={lineItem.product_id}>
-          <img
-            src={lineItem.product_images[0].image_url}
-            alt={lineItem.product_images[0].image_alt}
-            className="w-20 h-20 object-fit rounded-md"
-          />
-          <p>{lineItem.product_name}</p>
-          <p>£{lineItem.product_price}</p>
-          {/* <p>{lineItem.quantity}</p> */}
-          <div className="w-16">
-            <InputWithLabel
-              label="Quantity"
-              name="quantity"
-              id={String(lineItem.product_id)}
-              type="number"
-              state={quantityValues}
-              showLabel={false}
-              direction="column"
-              setState={setQuantityValues}
+      {products.map((lineItem) => {
+        if (
+          quantityValues[lineItem.product_id] <= 0 &&
+          String(quantityValues[lineItem.product_id]) !== ''
+        ) {
+          return null;
+        }
+        return (
+          <Fragment key={lineItem.product_id}>
+            <img
+              src={lineItem.product_images[0].image_url}
+              alt={lineItem.product_images[0].image_alt}
+              className="w-20 h-20 object-fit rounded-md"
             />
-          </div>
-          {/* change to dynamic with state instead of 1. Same as in above ibput */}
-          <p>£{lineItem.product_price * 1}</p>
-        </Fragment>
-      ))}
+            <p>{lineItem.product_name}</p>
+            <p>£{lineItem.product_price}</p>
+            <div className="w-16">
+              <InputWithLabel
+                label="Quantity"
+                name="quantity"
+                id={String(lineItem.product_id)}
+                type="number"
+                state={quantityValues}
+                showLabel={false}
+                direction="column"
+                setState={setQuantityValues}
+                min={0}
+              />
+            </div>
+            <p>
+              £{lineItem.product_price * quantityValues[lineItem.product_id]}
+            </p>
+          </Fragment>
+        );
+      })}
     </div>
   );
 }
