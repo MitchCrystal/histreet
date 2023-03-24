8b2e94c9c301cf13db0c0b78988c719ebf24870b
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-03-24 08:12:21 +0000

    feat(storefront checkoutcard primitive): checkout card primitive finished

--- apps/storefront/components/Checkoutcard.tsx
+++ apps/storefront/components/Checkoutcard.tsx
@@ -4,21 +4,18 @@ type Order = {
   name: string;
   price: number;
   quantity: number;
-};
+}[];
 
 
-type Orders = Order[];
-
-export default function Checkoutcard({CheckoutProps}:Order): JSX.Element {
-  function totalPrice(ord: Orders) {
+export default function Checkoutcard({values}:{values: Order}) {
+  function totalPrice(ord: Order) {
     let total = ord.reduce((accumulator, ord) => {
       return accumulator + ord.price;
     }, 0);
     return total;
   }
 
-  function totalQant(ord: Orders) {
-    console.log(ord);
+  function totalQant(ord: Order) {
     let total = ord.reduce((accumulator: any, ord: any) => {
       let number = Number(accumulator) + Number(ord.quantity);
       return number;
@@ -29,37 +26,33 @@ export default function Checkoutcard({CheckoutProps}:Order): JSX.Element {
   return (
     <div className="flex flex-col m-auto w-[80%] h-[80%] place-content-center ">
       <div className="grid grid-cols-4 w-full h-full place-content-center ">
-        <div></div>
-        <div className="flex place-content-center font-bold ">ITEMS</div>
-        <div></div>
-        <div className="flex place-content-center font-bold">QTY</div>
+        <div className="font-bold col-span-2">ITEMS</div>
+        <div className="font-bold col-span-2">QTY</div>
       </div>
       <div className="grid grid-row-4 my-2 w-full h-full ">
-        {CheckoutProps.map((order) => {
+        {values.map((order) => {
           return (
             <div className="grid grid-cols-4 my-2 w-full h-full" key={order.id}>
-              <div className="flex rounded h-10 w-10 ">
                 <img
                   src={order.image}
                   alt="order-image"
-                  className="flex rounded w-full h-full "
+                  className="rounded w-12 h-12 "
                 />
-              </div>
-              <div className="flex place-content-center ">
+              <div className="">
                 {order.name}
               </div>
-              <div className="flex place-content-center ">£{order.price}</div>
-              <div className="flex place-content-center ">x {order.quantity}</div>
+              <div className="">£{order.price}</div>
+              <div className="">x {order.quantity}</div>
             </div>
           );
         })}
       </div>
       <div className="grid grid-cols-4 w-full h-full place-content-center ">
-        <div></div>
-        <div className="flex place-content-center font-bold">Order Total</div>
-        <div className="flex place-content-center font-bold">£{totalPrice(CheckoutProps)}</div>
-        <div className="flex place-content-center font-bold">x{totalQant(CheckoutProps)}</div>
+        <div className="font-bold col-span-2">Order Total</div>
+        <div className="font-bold">£{totalPrice(values)}</div>
+        <div className="font-bold">x{totalQant(values)}</div>
       </div>
     </div>
   );
 }
+
--- apps/storefront/pages/[storeUrl]/[orderId]/order-confirmation.tsx
+++ apps/storefront/pages/[storeUrl]/[orderId]/order-confirmation.tsx
@@ -3,7 +3,7 @@ import Address from '../../../components/Address';
 import Button from '../../../components/Button';
 import { useRouter } from 'next/router';
 import Checkoutcard from '../../../components/Checkoutcard';
-
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
 
 export default function OrderConfirmation() {
   const router = useRouter();
@@ -28,10 +28,10 @@ export default function OrderConfirmation() {
       name: 'pants',
       price: 2323,
       quantity: 1,
-    },
+    }
   ];
   function onClick() {
-    router.push('/');
+    router.push(`/${router.query.storeUrl}`);
   }
 
   return (
@@ -81,7 +81,7 @@ export default function OrderConfirmation() {
             </div>
           </div>
           <div className="flex placement-content-center border-slate-200 border w-[100%] h-[100%]">
-            <Checkoutcard CheckoutProps={orders}/>
+            <Checkoutcard values={orders}/>
           </div>
         </div>
       </div>
@@ -93,3 +93,41 @@ export default function OrderConfirmation() {
     </div>
   );
 }
+
+// import { Address } from 'database';
+// type AddressWithError = Address | { error: boolean | string };
+// // import prisma from '../../../utils/prisma';
+
+// export async function getServerSideProps(){
+//   const order = async function handler(
+//     req: NextApiRequest,
+//     res: NextApiResponse<AddressWithError>
+//   ) {
+//     if (req.method === 'GET') {
+//       try {
+//         const { addressId } = req.query;
+//         if (!addressId || addressId === undefined) {
+//           return res.status(404).json({ error: 'Address not found' });
+//         }
+//         const address = await prisma.address.findUnique({
+//           where: {
+//             address_id: String(addressId),
+//           },
+//         });
+//         if (address) {
+//           res.status(200).json(address);
+//         } else {
+//           return res.status(404).json({ error: 'Address not found' });
+//         }
+//       } catch (error) {
+//         return res.status(500).json({ error: true });
+//       }
+//     }
+//   }
+  
+//   return {
+//     props: {
+//       order,
+//     },
+//   };
+// };
