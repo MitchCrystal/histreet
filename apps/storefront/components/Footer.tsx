de6192ebb7e5a812bc2bce95b8264cb1c99e17f6
YiYing Ko <ykocuriosity@gmail.com>
2023-03-27 16:03:44 +0100

    feat(footer): support email make dynamic
    suport email make dynamic

--- apps/storefront/components/Footer.tsx
+++ apps/storefront/components/Footer.tsx
@@ -1,4 +1,38 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+
+type StoreformEmail = {
+  id: string;
+  storeName: string;
+  supportEmail: string | null;
+};
+
 export default function Footer() {
+  const router = useRouter();
+  const storeUrl = router.query.storeUrl;
+
+  const [storeformEmail, setStoreformEmail] = useState<Record<string, any>>({
+    id: '',
+    supportEmail: '',
+  });
+
+  const { data: storeEmail }: UseQueryResult<Record<string, any>> = useQuery({
+    queryKey: ['storeEmail'],
+    queryFn: () => fetch(`/api/footer/${storeUrl}`).then((res) => res.json()),
+    enabled: !!router.isReady,
+    initialData: {},
+  });
+
+  useEffect(() => {
+    if (!storeEmail.storeName) {
+      return;
+    }
+    setStoreformEmail({
+      id: storeEmail.id,
+      supportEmail: storeEmail.supportEmail ?? '',
+    });
+  }, [storeEmail]);
   return (
     <>
       <footer className="border-t border-gray-300 py-8 px-6 bg-gray-800 text-white">
@@ -6,7 +40,8 @@ export default function Footer() {
           <p className="text-sm">
             {/* @TODO Insert dynamic email */}
             <span className="text-bold">Customer Support:</span>{' '}
-            example@example.com
+            {/* example@example.com */}
+            {storeformEmail.supportEmail}
           </p>
         </div>
       </footer>
