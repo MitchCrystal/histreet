0e25169f537fa8a6ae36ad14232acfdebadac2e1
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-03-28 17:26:28 +0100

    feat(admin testing): admin testing

--- cypress/e2e/admin.cy.ts
+++ cypress/e2e/admin.cy.ts
@@ -7,19 +7,18 @@ describe('From admin to sign in', () => {
 
   it('successfully loads images', () => {
     cy.visit('http://localhost:3001');
-    cy.get('img').should("exist");
+    cy.get('img').should('exist');
   });
 
   it('Redirect to sign in page', () => {
     cy.visit('http://localhost:3001');
     cy.contains('My Account').click();
-    cy.visit('http://localhost:3001/auth/sign-in');
-  })
+    cy.url().should('be.equal', 'http://localhost:3001/auth/sign-in');
+  });
 
   it('Redirect to sign up page', () => {
     cy.visit('http://localhost:3001');
     cy.contains('Sign Up Now').click();
-    cy.visit('http://localhost:3001/auth/sign-up');
-  })
-
+    cy.url().should('be.equal', 'http://localhost:3001/auth/sign-up');
+  });
 });
