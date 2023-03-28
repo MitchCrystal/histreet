1cae00c443237a956a8ed83d8a3d063989daeb2b
GeccoRhiguelNavalta <gecno@hotmail.co.uk>
2023-03-28 12:29:04 +0100

    feat(admin dashboard): dashboard almost complete

---
+++ apps/admin/components/LineChart.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export default function LineChart({options, data}:any) {
+  return <Line options={options} data={data} />;
+}
